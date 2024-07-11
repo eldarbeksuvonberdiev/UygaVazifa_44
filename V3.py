371948830cf384db4c78e6aa3f6b1a857caa3109
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2024-07-11 20:25:37 +0500

    Va nixoyat nimadir qildim

--- V3.py
+++ V3.py
@@ -1,1 +1,13 @@
-print("salom")
+def file_yoz():
+	print("Nechta foydalanuvchi kiritmoqchisiz: ")
+	soni = int(input("Soni: "))
+	print("Ma'lumotlar: Ism(Kim) Daraja(Middle) oylik(450) Bonus(100) bo'lim(1-Bo'lim) ")
+	for i in range(soni):
+		with open("It_programmers.txt","a") as f:
+			f.write(input() + " " + "\n")
+file_yoz()
+
+with open("It_programmers.txt", "r") as f:
+	malumotlar = f.read()
+	malumotlar = malumotlar.split()
+print(malumotlar)
