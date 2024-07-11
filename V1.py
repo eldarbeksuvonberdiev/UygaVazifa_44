8388326ce4649d32f0609f9ebca41fbea74be2a7
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2024-07-11 16:02:33 +0500

    Birinchi masala tayyor

--- V1.py
+++ V1.py
@@ -1,2 +1,11 @@
-print("Salom dunyo")
-print("Hello world")
+lim = int(input("Listga nechta qiymat kiritmoqchisiz? "))
+lst = []
+
+for i in range(lim):
+	lst.append(int(input(f"{i + 1}- qiymat: ")))
+for i in range(lim - 1):
+	if lst[i] > 0 and lst[i] == lst[i + 1] - 1:
+		print(lst[i], lst[i + 1])
+	elif lst[i] < 0 and lst[i] == lst[i + 1] + 1:
+		print(lst[i], lst[i + 1])
+
