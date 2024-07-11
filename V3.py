c9684eba82049774950769d2ae1af2ec42eaa0d9
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2024-07-11 20:57:15 +0500

    Ozgina chalasi bor lekin vaqtim yetmadi

--- V3.py
+++ V3.py
@@ -5,9 +5,31 @@ def file_yoz():
 	for i in range(soni):
 		with open("It_programmers.txt","a") as f:
 			f.write(input() + " " + "\n")
-file_yoz()
+#file_yoz()
 
 with open("It_programmers.txt", "r") as f:
-	malumotlar = f.read()
-	malumotlar = malumotlar.split()
-print(malumotlar)
+	malumotlar = f.read()[:-1]
+	malumotlar = malumotlar.split("\n")
+for ind, malumot in enumerate(malumotlar):
+	malumotlar[ind] = malumot.split(" ")[:-1]
+	malumotlar[ind][3] = int(malumotlar[ind][3])
+b1,b2,b3 = 0,0,0
+
+for malumot in malumotlar:
+	 _, _,  _, bonus, bolim = malumot
+"""	print(bonus)
+#	bolim = bolim.split("-")
+	gurux,_ = bolim
+	if gurux == 1:
+		b1 += bonus
+	elif gurux == 2:
+		b2 += bonus
+	else :
+		b3 += bonus
+"""
+if b1 > b2 > b3:
+	print("1 - bo'lim")
+elif b2 > b1 > b3:
+	print("2 bo'lim")
+else:
+	print("3 bo'lim")
